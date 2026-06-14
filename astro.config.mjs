78aae67511c62201d2420e160ad80f8d0b1770f1
furstblumier <seal@furst.blue>
2026-06-14 21:09:35 +0200

    Migrate Tailwind integration to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
@@ -9,9 +9,10 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind({
-    applyBaseStyles: true
-  }), sitemap(), mdx(), react(), icon()],
+  integrations: [sitemap(), mdx(), react(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro"
